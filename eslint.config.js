8d6619ffd0b6341df0dbd943d766840ff7e94cd8
furantuturu <fuwansuu@gmail.com>
2025-02-04 19:11:22 +0800

    added tanstack router eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pluginRouter from "@tanstack/eslint-plugin-router";
 
 export default tseslint.config(
+  ...pluginRouter.configs['flat/recommended'],
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],

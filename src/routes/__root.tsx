9fda6b17f8d8c1dad13587f970af71c16571cb4a
furantuturu <fuwansuu@gmail.com>
2025-02-04 19:18:42 +0800

    init main routes

---
+++ src/routes/__root.tsx
@@ -0,0 +1,40 @@
+import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import type { QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import ColorThemeButton from '../components/ColorThemeButton'
+
+interface QueryClientContext {
+    queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<QueryClientContext>()({
+    component: RootComponent,
+})
+
+function RootComponent() {
+    return (
+        <>
+            <nav className="bg-blue-400 dark:bg-yellow-400 ease-in-out duration-150">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-center md:justify-end mx-auto p-4">
+                    <ul className="flex font-medium text-xl mr-7 md:space-x-8 rtl:space-x-reverse">
+                        <li className="link-styles">
+                            <Link to="/" className="link-text-styles">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="link-styles">
+                            <Link to="/search" className="link-text-styles">
+                                Search Anime
+                            </Link>
+                        </li>
+                    </ul>
+                    <ColorThemeButton />
+                </div>
+            </nav>
+            <Outlet />
+            <ReactQueryDevtools buttonPosition='top-left' />
+            <TanStackRouterDevtools position='bottom-right' />
+        </>
+    )
+}

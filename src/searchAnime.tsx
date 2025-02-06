6e85833b7cc41036254e1cc0c3d66ea7d5eabac7
furantuturu <fuwansuu@gmail.com>
2025-02-06 19:16:55 +0800

    allows request to accept multiple keyword and return 20 max data

--- src/searchAnime.tsx
+++ src/searchAnime.tsx
@@ -1,14 +1,17 @@
 
-import { AnimeDataArray } from "./animeConstants";
+import { AnimeDataArray, AnimeData } from "./animeConstants";
 
 export const fetchAnime = async (searchString: string) => {
-    const res = await fetch('https://github.com/manami-project/anime-offline-database/raw/master/anime-offline-database-minified.json')
+    const res = await fetch('https://corsproxy.io/?url=https://github.com/manami-project/anime-offline-database/raw/master/anime-offline-database-minified.json')
     const data = await res.json()
 
     const animeDatas: AnimeDataArray = data.data
-    const filterData: AnimeDataArray = animeDatas.filter((user: any) => {
-        return searchString && user && user.name && user.name.toLowerCase().includes(searchString.toLowerCase())
+    const filterData: AnimeDataArray = animeDatas.filter((anime: AnimeData) => {
+        if (!searchString || !anime || !anime.title) return false
+
+        const keywords = searchString.trim().toLowerCase().split(' ')
+        return keywords.every((kw: string) => anime.title.toLowerCase().includes(kw))
     })
 
-    return filterData
+    return filterData.slice(0, 20)
 }

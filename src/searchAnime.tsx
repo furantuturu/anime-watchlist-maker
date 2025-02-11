6a6d965094dba9ea7f5256a75089ab0359b61402
furantuturu <fuwansuu@gmail.com>
2025-02-11 19:54:26 +0800

    applied new method of searching

--- src/searchAnime.tsx
+++ src/searchAnime.tsx
@@ -7,10 +7,13 @@ export const fetchAnime = async (searchString: string) => {
 
     const animeDatas: AnimeDataArray = data.data
     const filterData: AnimeDataArray = animeDatas.filter((anime: AnimeData) => {
-        if (!searchString || !anime || !anime.title) return false
+        if (!searchString || !anime || !anime.synonyms) return false
 
         const keywords = searchString.trim().toLowerCase().split(' ')
-        return keywords.every((kw: string) => anime.title.toLowerCase().includes(kw))
+        return keywords.every((kw: string) => anime.synonyms.find(title => {
+            const reg = new RegExp(kw, 'gi')
+            return title.match(reg)
+        }))
     })
 
     return filterData.slice(0, 20)

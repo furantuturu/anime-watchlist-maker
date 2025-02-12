a2a9ab0ccd530db36e0c7ae439a1dc9682a83f7a
furantuturu <fuwansuu@gmail.com>
2025-02-12 19:03:24 +0800

    applied the Tags and Sources components, added Add To Watchlist btn

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,16 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { animeQueryOptions } from "../searchAnimeQueryOptions";
 import { PropagateLoader } from "react-spinners";
-import { AnimeData } from "../animeConstants";
-import { memo } from "react";
+import { AnimeData, isLocalDark, isPreferColorSchemeDark } from "../constants";
+import { memo} from "react";
+import Tags from "./Tags";
+import Sources from "./Sources";
 
 type DBSearchString = { debouncedSearchString: string }
 
 const SearchResult = ({ debouncedSearchString }: DBSearchString) => {
     const { data, isFetching } = useQuery(animeQueryOptions(debouncedSearchString))
-    
-    const isLocalDark = localStorage.getItem('color-theme') === 'dark'
-    const isPreferColorSchemeDark = (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
     
     return (
         <>
@@ -20,37 +19,30 @@ const SearchResult = ({ debouncedSearchString }: DBSearchString) => {
                 {data?.map((anime: AnimeData) => {
                     return (
                         <li key={anime.title} className="card-li-styles">
-                            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-80 md:rounded-none md:rounded-s-lg" src={anime.picture} alt={anime.title} />
-                            <div className="flex flex-col justify-center text-center md:text-left p-5 leading-normal">
-                                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{anime.title}</h3>
-                                <div>
+                            <img className="card-li-img-styles" src={anime.picture} alt={anime.title} />
+                            <div className="flex grow-1 flex-col justify-center text-center md:text-left p-5 leading-normal">
+                                <div className="mb-2 flex flex-col md:flex-row md:justify-between">
+                                    <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-1 md:m-0">{anime.title}</h3>
+                                    <button className="add-watchlist-btn-styles">Add To Watchlist</button>
+                                </div>
+                                <div className="mb-2">
                                     <h5 className="font-bold">Type</h5>
                                     <p>{anime.type}</p>
                                 </div>
-                                <div>
+                                <div className="mb-2">
                                     <h5 className="font-bold">Episodes</h5>
                                     <p>{anime.episodes}</p>
                                 </div>
-                                <div>
+                                <div className="mb-2">
                                     <h5 className="font-bold">Status</h5>
                                     <p>{anime.status}</p>
                                 </div>
-                                <div>
+                                <div className="mb-2">
                                     <h5 className="font-bold">Season</h5>
                                     <p>{anime.animeSeason.season} {anime.animeSeason.year}</p>
                                 </div>
-                                <div>
-                                    <h5 className="font-bold">Tags</h5>
-                                    <div className="leading-relaxed">
-                                        {anime.tags.map(tag => <span className="pr-2 pb-1 hover:underline underline-offset-4" key={tag}>{tag}</span>)}
-                                    </div>
-                                </div>
-                                <div>
-                                    <h5 className="font-bold">Sources</h5>
-                                    <div className="leading-relaxed">
-                                        {anime.sources.map(source => <li><a href={source} target="_blank"></a>{source}</li>)}
-                                    </div>
-                                </div>
+                                <Tags tags={anime.tags} />
+                                <Sources sources={anime.sources} />
                             </div>
                         </li>
                     )
@@ -60,10 +52,4 @@ const SearchResult = ({ debouncedSearchString }: DBSearchString) => {
     )
 }
 
-const arePropsEqual = (prevProp: DBSearchString, nextProp: DBSearchString) => {
-    return prevProp.debouncedSearchString === nextProp.debouncedSearchString
-}
-
-const memoizedSearchResult = memo(SearchResult, arePropsEqual) 
-
-export default memoizedSearchResult
+export default memo(SearchResult)

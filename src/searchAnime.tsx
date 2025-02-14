fbced6cb825fe8fff5272ab30c53beb89076f7fa
furantuturu <fuwansuu@gmail.com>
2025-02-14 19:11:41 +0800

    excluded animes that has adult tag for now

--- src/searchAnime.tsx
+++ src/searchAnime.tsx
@@ -7,13 +7,13 @@ export const fetchAnime = async (searchString: string) => {
 
     const animeDatas: AnimeDataArray = data.data
     const filterData: AnimeDataArray = animeDatas.filter((anime: AnimeData) => {
-        if (!searchString || !anime || !anime.synonyms) return false
+        if (!searchString || !anime) return false
 
         const keywords = searchString.trim().toLowerCase().split(' ')
         return keywords.every((kw: string) => anime.synonyms.find(title => {
             const reg = new RegExp(kw, 'gi')
             return title.match(reg)
-        }))
+        })) && !anime.tags.includes("adult")
     })
 
     return filterData.slice(0, 20)

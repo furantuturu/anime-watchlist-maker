9294f6b36f7ab833e5ddf2c1a309b55b8429df35
furantuturu <fuwansuu@gmail.com>
2025-02-17 20:51:34 +0800

    card component for expanded anime data response

---
+++ src/components/ExpandedAnimeCard.tsx
@@ -0,0 +1,16 @@
+import AnimeCard from "./AnimeCard"
+import { AnimeData } from "../constants";
+
+type Data = { data: AnimeData[] | undefined }
+
+const ExpandedAnimeCard = ({ data }: Data) => {
+    return (
+        <>
+            {data?.slice(5, 20).map((anime: AnimeData) => {
+                return <AnimeCard key={anime.title} anime={anime} />
+            })}
+        </>
+    )
+}
+
+export default ExpandedAnimeCard

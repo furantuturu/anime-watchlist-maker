a94121c75a9d6477c8fc25826918c99800be5515
furantuturu <fuwansuu@gmail.com>
2025-02-17 20:53:24 +0800

    AnimeData[] to rename from AnimeDataArray

--- src/searchAnime.tsx
+++ src/searchAnime.tsx
@@ -1,12 +1,12 @@
 
-import { AnimeDataArray, AnimeData } from "./constants";
+import { AnimeData } from "./constants";
 
 export const fetchAnime = async (searchString: string) => {
     const res = await fetch('https://corsproxy.io/?url=https://github.com/manami-project/anime-offline-database/raw/master/anime-offline-database-minified.json')
     const data = await res.json()
 
-    const animeDatas: AnimeDataArray = data.data
-    const filterData: AnimeDataArray = animeDatas.filter((anime: AnimeData) => {
+    const animeDatas: AnimeData[] = data.data
+    const filterData: AnimeData[] = animeDatas.filter((anime: AnimeData) => {
         if (!searchString || !anime) return false
 
         const keywords = searchString.trim().toLowerCase().split(' ')

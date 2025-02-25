4797242265a4fa1b89e8084321296be5aec66566
furantuturu <fuwansuu@gmail.com>
2025-02-25 17:20:35 +0800

    component for every anime type for the dropdown

---
+++ src/components/AnimeTypeCBox.tsx
@@ -0,0 +1,16 @@
+import { memo } from "react"
+
+type AnimeType = { animeType: string }
+
+const AnimeTypeCBox = ({ animeType }: AnimeType) => {
+    return (
+        <li className="p-2 hover:bg-gray-200 dark:hover:bg-new-dark rounded-md">
+            <div className="flex items-center px-2">
+                <input id={`only-${animeType}`} type="checkbox" className="w-4 h-4 cursor-pointer" />
+                <label htmlFor={`only-${animeType}`} className="dd-cbox-label-styles">{animeType.slice(0, 1).toUpperCase() + animeType.slice(1)}</label>
+            </div>
+        </li>
+    )
+}
+
+export default memo(AnimeTypeCBox)

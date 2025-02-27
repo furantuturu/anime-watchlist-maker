115724ee10595e34bddae68687e1603b07c73958
furantuturu <fuwansuu@gmail.com>
2025-02-27 20:27:40 +0800

    added intersection obv or baack to top btn

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,8 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import type { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ColorThemeButton from '../components/ColorThemeButton'
-import { RefObject, useRef } from 'react'
+import { RefObject, useCallback, useRef } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 interface QueryClientContext {
     queryClient: QueryClient
@@ -15,39 +16,43 @@ export const Route = createRootRouteWithContext<QueryClientContext>()({
 
 function RootComponent() {
     const navRef: RefObject<HTMLElement> = useRef(null)
+    const { ref, inView } = useInView()
 
-    function backToTop() {
+    const backToTop = useCallback(() => {
         const navRect = navRef.current!.getBoundingClientRect()
         window.scrollTo({
             left: navRect.left + window.scrollX,
             top: navRect.top + window.scrollY,
             behavior: 'smooth'
         })
-    }
+    }, [])
+
 
     return (
         <>
-            <nav ref={navRef} className="bg-blue-400 dark:bg-yellow-400">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-center md:justify-end mx-auto p-4">
-                    <ul className="flex font-medium text-xl mr-7 md:space-x-8 rtl:space-x-reverse">
-                        <li className="link-styles">
-                            <Link to="/" className="link-text-styles">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="link-styles">
-                            <Link to="/search" className="link-text-styles">
-                                Search Anime
-                            </Link>
-                        </li>
-                    </ul>
-                    <ColorThemeButton />
-                </div>
-            </nav>
+            <div ref={ref}>
+                <nav ref={navRef} className="bg-blue-400 dark:bg-yellow-400">
+                    <div className="max-w-screen-xl flex flex-wrap items-center justify-center md:justify-end mx-auto p-4">
+                        <ul className="flex font-medium text-xl mr-7 md:space-x-8 rtl:space-x-reverse">
+                            <li className="link-styles">
+                                <Link to="/" className="link-text-styles">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="link-styles">
+                                <Link to="/search" className="link-text-styles">
+                                    Search Anime
+                                </Link>
+                            </li>
+                        </ul>
+                        <ColorThemeButton />
+                    </div>
+                </nav>
+            </div>
             <Outlet />
             <ReactQueryDevtools buttonPosition='top-left' />
             {/* <TanStackRouterDevtools position='bottom-right' /> */}
-            <button className="back-to-top-styles" onClick={backToTop}>Back To Top</button>
+            <button className={`${!inView ? 'visible translate-y-0' : 'invisible translate-y-50'} back-to-top-styles`} onClick={backToTop}>Back To Top</button>
         </>
     )
 }

5a0a44e72eb80e6e745c554fa1036da6f0929d64
furantuturu <fuwansuu@gmail.com>
2025-03-03 16:02:37 +0800

    replaced ul ele to a fragment

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -20,18 +20,18 @@ const SearchResult = ({ searchString }: DBSearchString) => {
     if (data!.length < 1) return <p className="text-center font-bold p-2 text-blue-400 dark:text-yellow-300">Anime not found...</p>
 
     return (
-        <ul>
+        <>
             {data!.slice(0, 5).map((anime: AnimeData) => {
                 return <AnimeCard key={anime.title} anime={anime} />
             })}
             {expand && data!.length >= 5 && <hr className="my-10 text-blue-300 dark:text-yellow-300" />}
-            {expand && <ExpandedAnimeCard data={data} />}
+                {expand && <ExpandedAnimeCard data={data} />}
             {data!.length >= 5 && <div className={`text-center mb-10 ${!data ? "hidden" : ''}`}>
                 <small onClick={() => setExpand(prev => !prev)} className="font-bold p-2 text-blue-400 dark:text-yellow-300 cursor-pointer hover:underline">
                     {!expand ? "Show More..." : "Show Less..."}
                 </small>
             </div>}
-        </ul>
+        </>
     )
 }
 
